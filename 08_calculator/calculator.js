52213d90e6f227e70fa05efbe6ef8b9922d1f885
semighoti <semighoti@sheet2odoo.com>
2024-04-17 20:04:28 -0700

    Changes to be committed: 	modified:   08_calculator/calculator.js 	modified:   08_calculator/calculator.spec.js 	modified:   09_palindromes/palindromes.js 	modified:   09_palindromes/palindromes.spec.js 	modified:   09_palindromes/solution/palindromes-solution.js

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,36 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(ar) {
+	return ar.reduce((total, current) => total + current, 0)
 };
 
-const multiply = function() {
-
+const multiply = function(ar) {
+  return ar.reduce((val, current) => val * current)
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return Math.pow(a ,b);
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+	if (n ===0) return 1;
+  let total = 1
+  for (let i = n; i >0; i--){
+    total *= i;
+  }
+  return total;
 };
+
+const recursiveFactorial = function (n){
+  if (n === 0) {return 1;}
+  return n * recursiveFactorial(n - 1);
+}
 
 // Do not edit below this line
 module.exports = {
--- 09_palindromes/solution/palindromes-solution.js
+++ 09_palindromes/solution/palindromes-solution.js
@@ -1,8 +1,10 @@
 const palindromes = function (string) {
-  // Since we only consider letters and numbers, create a variable containing all valid characters
+  // Since we only consider letters and numbers, 
+  // create a variable containing all valid characters
   const alphanumerical = 'abcdefghijklmnopqrstuvwxyz0123456789';
 
-  // Convert to lowercase, split to array of individual characters, filter only valid characters, then rejoin as new string
+  // Convert to lowercase, split to array of individual characters, 
+  // filter only valid characters, then rejoin as new string
   const cleanedString = string
     .toLowerCase()
     .split('')
@@ -12,7 +14,8 @@ const palindromes = function (string) {
   // Create a new reversed string for comparison
   const reversedString = cleanedString.split('').reverse().join('');
 
-  // Return the outcome of the comparison which will either be true or false
+  // Return the outcome of the comparison 
+  // which will either be true or false
   return cleanedString === reversedString;
 };
 
